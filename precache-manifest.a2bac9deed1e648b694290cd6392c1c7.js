7b5e948a22081f7870bbe45592d2b0575b34d4ed
negin-titanium <neginshahany@gmail.com>
2020-11-14 18:47:19 +0330

    Updates

---
+++ precache-manifest.a2bac9deed1e648b694290cd6392c1c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1470b52db09b50c7332c",
+    "url": "/maktabkhooneh-todoApp/static/css/main.4efb491e.chunk.css"
+  },
+  {
+    "revision": "1470b52db09b50c7332c",
+    "url": "/maktabkhooneh-todoApp/static/js/main.1470b52d.chunk.js"
+  },
+  {
+    "revision": "5c497014ad9491489323",
+    "url": "/maktabkhooneh-todoApp/static/js/1.5c497014.chunk.js"
+  },
+  {
+    "revision": "f5a3557f08b145285191",
+    "url": "/maktabkhooneh-todoApp/static/js/runtime~main.f5a3557f.js"
+  },
+  {
+    "revision": "ad2ef2b8390134223919cc9489c27287",
+    "url": "/maktabkhooneh-todoApp/index.html"
+  }
+];
